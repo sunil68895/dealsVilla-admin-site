ebf54865046d400c6fa3f464627245d207e3ed9e
sunil sharma <sunil68895@gmail.com>
2021-05-23 11:16:33 +0530

    update urlConfig

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 //const baseUrl = process.env.API || 
-const baseUrl="https://dealsvilla-backend.herokuapp.com/";
+const baseUrl="https://dealsvilla-backend.herokuapp.com";
 // const baseUrl = "http://localhost:2000";
 
 export const api = `${baseUrl}/api`;
